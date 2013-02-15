7c78cbf450afe295412595408d014c7654ba32d2
Michael Hart <michaelhart@michaelhart.me>
2013-02-15 00:13:30 -0800

    Cleaned up Tutorial widget slightly
    - Async loaders moved to HTML
    - fadeIn increased to 5 seconds

--- widgets/tutorial/tutorial.js
+++ widgets/tutorial/tutorial.js
@@ -1,28 +1,5 @@
-$(window).load(function() {
-  loadPlusOneScript();
-  loadTwitterScript();
-});
-
-function loadPlusOneScript() {
-  var po = document.createElement("script");
-  po.type = "text/javascript";
-  po.async = true;
-  po.src = "https://apis.google.com/js/plusone.js";
-  var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);
-}
-
-function loadTwitterScript() {
-  var twitterScriptTag = document.createElement("script");
-  twitterScriptTag.type = "text/javascript";
-  twitterScriptTag.async = true;
-  twitterScriptTag.src = "https://platform.twitter.com/widgets.js";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(twitterScriptTag, s);
-}
-
-
 $(document).on("click", "#tutorial-start", function(event) {
-  $(".social").fadeIn(2000);
+  $(".social").fadeIn(5000);
   if ( parent.startTutorial ) {
     $(document).trigger("tutorial-reset");
     parent.startTutorial();
